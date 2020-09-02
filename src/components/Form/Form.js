bc895eabd07132890702f776cee1377dd31f8403
Aaron Burris-DeBoskey <abdeboskey@gmail.com>
2020-09-02 10:57:46 -0600

    Create handle input and reset inputs methods

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -10,40 +10,55 @@ class Form extends Component {
     }
   }
 
+  handleInput(event) {
+    const inputName = event.target.id
+    const inputValue = event.target.value
+    this.setState({ [inputName]: inputValue })
+  }
+
+  resetInputs() {
+    this.setState({
+      songName: '',
+      artistName: '',
+      link: ''
+    })
+
+  }
+
   render() {
     return (
       <form
-        className="SongForm"
+        className='SongForm'
         onSubmit={(event) => {
-          this.props.addSong(event, this.state.comment);
-          this.resetInputs();
+          this.props.addSong(event, this.state.songName, this.state.artistName, this.state.link)
+          this.resetInputs()
         }}
       >
         <input
-          className="songName-input"
-          type="text"
-          name='songName'
-          placeholder="Song Name"
+          className='songName-input'
+          type='text'
+          id='songName'
+          placeholder='Song Name'
           value={this.state.songName}
           onChange={(event) => this.handleInput(event)}
         />
         <input
-          className="artistName-input"
-          type="text"
-          name='artistName'
-          placeholder="Artist Name"
+          className='artistName-input'
+          type='text'
+          id='artistName'
+          placeholder='Artist Name'
           value={this.state.artistName}
           onChange={(event) => this.handleInput(event)}
         />
         <input
-          className="link-input"
-          type="text"
-          name='link'
-          placeholder="Song Link"
+          className='link-input'
+          type='text'
+          id='link'
+          placeholder='Song Link'
           value={this.state.link}
           onChange={(event) => this.handleInput(event)}
         />
-        <button className="addSong-button" type="submit">
+        <button className='addSong-button' type='submit'>
           Add Song
         </button>
       </form>
