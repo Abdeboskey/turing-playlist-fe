20a96e4b352b8997b3852b13721e777c0a81f896
Aaron Burris-DeBoskey <abdeboskey@gmail.com>
2020-09-02 10:34:10 -0600

    Create song component

--- src/components/Song/Song.js
+++ src/components/Song/Song.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import './Song.css'
 
-const Song = () => {
-  <article>
-    
-  </article>
+const Song = ({ songName, artistName, link}) => {
+  return (
+    <article className='Song'>
+      <h2>{songName}</h2>
+      <p>{artistName}</p>
+      <a href={link} target='_blank'>Listen Here</a>
+    </article>
+  )
 }
 
 export default Song
